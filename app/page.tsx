3bc04cd7194700167c3f5eb4c4b688151e3e73b8
맥동우 <t6u80o@naver.com>
2025-02-01 13:39:29 +0900

    group modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,35 @@ export default function Home() {
         {["kim", "me", "you", "yourself", undefined].map((person, index) => (
           <div
             key={index}
-            className="flex items-center gap-5 p-2.5 odd:bg-gray-100 even:bg-cyan-50 rounded-xl"
+            className="group flex items-center gap-5 p-2.5 odd:bg-gray-100 even:bg-cyan-50 rounded-xl"
           >
             <div className="size-10 rounded-full bg-blue-400" />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full animate-pulse empty:bg-gray-400">
+            <span
+              className="text-lg font-medium 
+              empty:w-24 empty:h-5 empty:rounded-full animate-pulse empty:bg-gray-400 
+             group-hover:text-red-500"
+            >
               {person}
             </span>
             <div className="size-6 bg-red-500 text-white flex justify-center rounded-full relative">
               <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute hover:animate-ping" />
+              <div className="size-6 bg-red-500 rounded-full absolute group-hover:animate-ping" />
             </div>
           </div>
         ))}
+        <div className="flex flex-col gap-3 group">
+          <input
+            className="w-full bg-gray-200 px-2"
+            placeholder="write email"
+            type="text"
+          />
+          <span className="group-focus-within:block hidden">
+            Make sure it is a invaild email
+          </span>
+          <button className="bg-blue-500 rounded-full text-white px-2">
+            submit
+          </button>
+        </div>
       </div>
     </main>
   );
