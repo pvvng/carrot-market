1d3ac9ce9d0072c2a107e81c0e07774e2dd22f30
맥동우 <t6u80o@naver.com>
2025-02-01 13:20:19 +0900

    list and animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,22 @@ export default function Home() {
   // mac 자동완성 커맨드 -> cmd + i
   return (
     <main className="bg-gray-100 h-screen flex justify-center items-center p-5">
-      <div
-        className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2
-        *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-200"
-      >
-        <input
-          className="w-full rounded-full py-3 bg-gray-200 pl-5
-          ring ring-transparent focus:ring-green-500 focus:ring-offset-2
-          placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          placeholder="Email Address"
-          required
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is Required
-        </span>
-        <button
-          className="bg-black text-white py-2 rounded-full font-medium
-        transition-transform active:scale-90 md:px-10"
-        >
-          Login
-        </button>
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-3">
+        {["kim", "me", "you", "yourself", undefined].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 p-2.5 odd:bg-gray-100 even:bg-cyan-50 rounded-xl"
+          >
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full animate-pulse empty:bg-gray-400">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute hover:animate-ping" />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
