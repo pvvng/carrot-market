13a245d7af5b431358b9823285fa76655584f4b1
맥동우 <t6u80o@naver.com>
2025-02-05 22:49:55 +0900

    schemas and model

---
+++ prisma/migrations/20250205133805_add_user/migration.sql
@@ -0,0 +1,26 @@
+-- npx prisma migrate dev 후 add_<modal name> 입력하고 생성된 sql문
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "password" TEXT,
+    "phone" TEXT,
+    "github_id" TEXT,
+    "avatar" TEXT,
+    "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_github_id_key" ON "User"("github_id");
